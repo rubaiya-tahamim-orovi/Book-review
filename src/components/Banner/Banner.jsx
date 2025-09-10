c53ba28acf67f1806baaadc096fb8184e957d2e9
orovi7 <rubaiyatahamimorovi@gmail.com>
2025-09-10 15:14:11 +0600

    text-xs

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
         <div className='flex justify-around items-center md:mx-0 mx-4  rounded-xl py-7 bg-[#1313130D] my-7'>
             <div className='space-y-4 md:m-0 mx-4'>
                 <h2 className='md:text-4xl text-2xl font-bold'>Books to freshen up your <br /> bookshelf</h2>
-                <Link to="/listedBooks"> <button className='bg-[#23BE0A]
+                <Link to="/listedBooks"> <button className='bg-[#23BE0A] md:text-base text-xs
                 cursor-pointer text-white font-bold md:px-4 px-2 py-2 rounded-md'>View The List</button></Link>
             </div>
             <div>
